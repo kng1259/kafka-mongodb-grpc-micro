cf27f47508311ef79c587d35e94cfe1b8aa6ff2d
Ubuntu <ubuntu@poc4k-tsnode1b.ovng.dev.myovcloud.com>
2025-09-25 10:07:53 +0000

    custom registry

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-docker buildx build -t kng1259/kmgm-consumer ./consumer
-docker buildx build -t kng1259/kmgm-producer ./producer
-docker push kng1259/kmgm-consumer
-docker push kng1259/kmgm-producer
+# docker buildx build -t kng1259/kmgm-consumer ./consumer
+# docker buildx build -t kng1259/kmgm-producer ./producer
+# docker push kng1259/kmgm-consumer
+# docker push kng1259/kmgm-producer
+
+docker buildx build -t poc4k-tsnode1b.ovng.dev.myovcloud.com:8444/kmgm-consumer:latest consumer/
+docker buildx build -t poc4k-tsnode1b.ovng.dev.myovcloud.com:8444/kmgm-producer:latest producer/
+docker push poc4k-tsnode1b.ovng.dev.myovcloud.com:8444/kmgm-consumer:latest
+docker push poc4k-tsnode1b.ovng.dev.myovcloud.com:8444/kmgm-producer:latest
